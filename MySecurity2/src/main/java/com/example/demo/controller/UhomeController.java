5f4b5223d982168e6dff683c93034ab9e697458d
seiyasannda <seiya.mario1212@icloud.com>
2023-11-15 11:01:10 +0900

    ユーザーのホーム画面と検索

--- MySecurity2/src/main/java/com/example/demo/controller/UhomeController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/UhomeController.java
@@ -1,12 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+@Controller
 public class UhomeController {
-	@RequestMapping(path = "/uhome", method = RequestMethod.GET)
-	public String viewPage() {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
-		return "uhome";
-	}
+	   @RequestMapping(path = "/uhome", method = RequestMethod.GET)
+	    public String viewPage(Model model) {
+	        // Fetch data from the database, assuming you have a table named 'items'
+	        List<Map<String, Object>> items = jdbcTemplate.queryForList("SELECT * FROM vitems");
+
+	        // Pass the data to the HTML file
+	        model.addAttribute("items", items);
+
+	        return "uhome";
+	    }
+
 }
