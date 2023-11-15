ff490c774fff3f39a3ebe6caedd0bc895f878e90
S0519 <S0519@19S0519>
2023-11-15 11:09:13 +0900

    itemview追加

---
+++ MySecurity2/src/main/java/com/example/demo/controller/VitemEditController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class VitemEditController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(path = "/viewItem/{itemId}", method = RequestMethod.GET)
+    public String viewItem(@PathVariable Long itemId, Model model) {
+        // Fetch data for a single item from the database
+        Map<String, Object> item = jdbcTemplate.queryForMap("SELECT * FROM vitems WHERE id = ?", itemId);
+
+        // Pass the data to the HTML file
+        model.addAttribute("item", item);
+
+        return "viewItem";
+    }
+}
