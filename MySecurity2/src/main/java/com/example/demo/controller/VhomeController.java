86fae2d294a069457f3418c2a63c23bae250d842
S0519 <S0519@19S0519>
2023-12-08 13:50:10 +0900

    login

--- MySecurity2/src/main/java/com/example/demo/controller/VhomeController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/VhomeController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("id")
 public class VhomeController {
 
 	@Autowired
--- MySecurity2/src/main/java/com/example/demo/controller/VloginController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/VloginController.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@SessionAttributes("id")
 public class VloginController {
 	@Autowired
     JdbcTemplate jdbcTemplate;
