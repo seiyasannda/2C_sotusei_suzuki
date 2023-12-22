7519768aa72f499dc46b0abda2d59320a7bc7012
seiyasannda <seiya.mario1212@icloud.com>
2023-12-22 14:00:34 +0900

    ユーザー画面

--- MySecurity2/src/main/java/com/example/demo/controller/UsdController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/UsdController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,11 +19,13 @@ public class UsdController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
-	@RequestMapping(path = "/usd", method = RequestMethod.GET)
-	public String viewPage(Model model) {
+	@RequestMapping(path = "/usd/{stores}", method = RequestMethod.GET)
+	public String viewPage(@PathVariable String stores,Model model) {
 
 		List<Map<String, Object>> items = jdbcTemplate.queryForList("SELECT * FROM vitems");
-		List<Map<String, Object>> store = jdbcTemplate.queryForList("SELECT * FROM vstoretable");
+		List<Map<String, Object>> store = jdbcTemplate.queryForList("SELECT * FROM vstoretable WHERE Sname = ?",stores);
+
+		
         // Pass the data to the HTML file
         model.addAttribute("items", items);
         model.addAttribute("store", store);
--- MySecurity2/src/main/java/com/example/demo/controller/UsiController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/UsiController.java
@@ -1,14 +1,72 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UsiController {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping(path = "/usi", method = RequestMethod.GET)
 	public String viewPage() {
 
 		return "usi";
 	}
+	@RequestMapping(path = "/usi", method = RequestMethod.POST)
+	public String searchProducts(Model model, String productName, String discountPriceStr, String genre, String expirationDate) {
+	    List<Map<String, Object>> items;
+	    String query = "SELECT * FROM vitems";
+	    List<Object> params = new ArrayList<>();
+
+	    if (productName != null && !productName.isEmpty()) {
+	        query += " WHERE productName LIKE ?";
+	        params.add("%" + productName + "%");
+	    }
+	    if (discountPriceStr != null && !discountPriceStr.isEmpty()) {
+	        try {
+	            double discountPrice = Double.parseDouble(discountPriceStr);
+	            if (query.contains("WHERE")) {
+	                query += " AND price < ?"; // 値段が検索した値未満のものを取得する条件
+	            } else {
+	                query += " WHERE price < ?";
+	            }
+	            params.add(discountPrice);
+	        } catch (NumberFormatException e) {
+	            // 例外処理
+	        }
+	    }
+	    if (genre != null && !genre.isEmpty()) {
+	        if (query.contains("WHERE")) {
+	            query += " AND genre = ?";
+	        } else {
+	            query += " WHERE genre = ?";
+	        }
+	        params.add(genre);
+	    }
+	    if (expirationDate != null && !expirationDate.isEmpty()) {
+	        if (query.contains("WHERE")) {
+	            query += " AND expirationDate < ?"; // expirationDateが入力した日より前のものを取得する条件
+	        } else {
+	            query += " WHERE expirationDate < ?";
+	        }
+	        params.add(expirationDate);
+	    }
+
+	    items = jdbcTemplate.queryForList(query, params.toArray());
+	    List<Map<String, Object>> store = jdbcTemplate.queryForList("SELECT * FROM vstoretable");
+
+	    model.addAttribute("items", items);
+	    model.addAttribute("store", store);
+	    return "uhome";
+	}
 }
+
