6eef8d9cf90c10ca70a6426b3e4396de9e596648
seiyasannda <seiya.mario1212@icloud.com>
2024-01-25 13:56:17 +0900

    a

--- MySecurity2/src/main/java/com/example/demo/controller/UhomeController.java
+++ MySecurity2/src/main/java/com/example/demo/controller/UhomeController.java
@@ -7,23 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UhomeController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-
+	@GetMapping("/geocode")
+	
 	   @RequestMapping(path = "/uhome", method = RequestMethod.GET)
 	    public String viewPage(Model model) {
 	        // Fetch data from the database, assuming you have a table named 'items'
 	        List<Map<String, Object>> items = jdbcTemplate.queryForList("SELECT * FROM vitems");
 	        
 	        List<Map<String, Object>> store = jdbcTemplate.queryForList("SELECT * FROM vstoretable");
+	        
+	        List<String> addresses = jdbcTemplate.queryForList(
+	    		    "SELECT CONCAT(prefecture, ' - ', city, ' - ', address1, ' - ', address2) " +
+	    		    "FROM vstoretable", String.class);
 
 	        // Pass the data to the HTML file
 	        model.addAttribute("items", items);
 	        model.addAttribute("store", store);
+	        model.addAttribute("addresses", addresses);
 
 	        return "uhome";
 	    }
